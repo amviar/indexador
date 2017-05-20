45cdd60ba32ce9d18ebc00e892f9ffa459716d49
anero <anero79@gmail.com>
2017-05-20 17:28:40 -0300

    Loguear a STDOUT y con nivel DEBUG

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,8 @@ module Indexador
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.log_level = 'DEBUG'
+    config.logger = Logger.new(STDOUT)
   end
 end
